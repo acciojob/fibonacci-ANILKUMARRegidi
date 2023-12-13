1ae9dc14b406611ecb77fc9d9ececc36399a74a7
ANILKUMARRegidi <129140166+ANILKUMARRegidi@users.noreply.github.com>
2023-12-13 12:22:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-function fibonacci(num) {
-// your code here
-}
-
-module.exports = fibonacci;
+ function fibonacci(num) {
+    let prev = 0;
+    let current = 1;
+  
+    for (let i = 2; i <= num; i++) {
+      let fib = prev + current;
+      prev = current;
+      current = fib;
+    }
+  
+    return prev; // Return the Fibonacci number at num - 1 index
+  }
+  const result = fibonacci(num);
+  console.log(result);
+  module.exports = fibonacci;
+  
